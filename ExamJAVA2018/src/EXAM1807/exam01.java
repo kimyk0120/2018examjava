747b0ff4fd414d66e208585e31b3c4b25bcd4211
YoungKim <kimyk0120@hotmail.com>
2018-07-05 15:42:04 +0900

    Signed-off-by: YoungKim <kimyk0120@hotmail.com>

--- ExamJAVA2018/src/EXAM1807/exam01.java
+++ ExamJAVA2018/src/EXAM1807/exam01.java
@@ -9,8 +9,10 @@ public class exam01 {
 		
 		System.out.println("main called");
 		
-		System.out.println("test1");
-		System.out.println("test2");
+		System.out.println("git test1");
+		System.out.println("git test2");
+		System.out.println("git test3");
+		System.out.println("git test4");
 		
 		
 	}//.main

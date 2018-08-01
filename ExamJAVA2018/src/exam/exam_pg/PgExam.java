9e2d18fce547225819b66c7eaa450a216086978a
YoungKim <kimyk0120@hotmail.com>
2018-08-01 11:05:54 +0900

    Signed-off-by: YoungKim <kimyk0120@hotmail.com>

---
+++ ExamJAVA2018/src/exam/exam_pg/PgExam.java
@@ -0,0 +1,19 @@
+package exam.exam_pg;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+public class PgExam {
+
+	
+	public static void main(String[] args) throws IOException {
+		
+		String projectPath = System.getProperty("user.dir");
+		System.out.println(projectPath);
+		
+		FileInputStream fis=new FileInputStream(projectPath+"/src/exam/exam_pg/a.xlsx");
+		
+	}
+	
+}//.class
